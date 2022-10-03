ff7b4b858c33448f291636574c7caf008d57ca69
mitch <mitchell.n@outlook.com>
2022-10-03 17:05:26 +0200

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
-import { resolve } from 'path'
+const path = require('path')
 
-export const entry = './src/index.js'
-export const output = {
-    path: resolve(__dirname, 'dist'),
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  devtool: 'eval-source-map',
+  watch: true,
 }
-export const devtool = 'eval-source-map'
-export const watch = true
